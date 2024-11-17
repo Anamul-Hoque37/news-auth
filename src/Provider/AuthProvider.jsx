649180e3555677f083687b3243943237a860c8e2
anamul hoque <anamul.eee73@gmail.com>
2024-11-17 17:14:24 +0600

    added final

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,19 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/Firebase.config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const AuthContext = createContext();
 const auth = getAuth (app);
+
 const AuthProvider = ({children}) => {
     const [user, setUser]= useState(null);
+
     const createNewUser =(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
     };
+
+    const userLogin = (email, password)=> {
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const logOut=()=>{
+        return signOut(auth);
+    }
+
     const authInfo = {
         user,
         setUser,
         createNewUser,
+        logOut,
+        userLogin,
     };
+
+    useEffect(()=>{
+        const Unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+        });
+        return ()=>{
+            Unsubscribe();
+        }
+    })
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,22 +1,40 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Login = () => {
+    const {userLogin, setUser}= useContext(AuthContext)
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const email =form.email.value;
+        const password =form.password.value;
+        console.log(email,password);
+        userLogin(email,password)
+        .then(result=>{
+            const user=result.user
+            setUser(user)
+        })
+        .catch((error)=>{
+            alert(error.code);
+        })
+    };
     return (
         <div>
             <div className="card bg-base-100 w-2/4 mx-auto shrink-0 shadow-2xl">
-                <form className="card-body">
+                <form onSubmit={handleSubmit} className="card-body">
                     <h1 className="text-2xl font-bold text-center">Login Your Account</h1>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
